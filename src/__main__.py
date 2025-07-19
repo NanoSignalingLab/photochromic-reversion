bd92ebb5e29343c2779cd7279df160592c41e706
Philip Schulz <pp.schulz@proton.me>
2025-07-19 13:57:29 +0200

    run calc sta main function from new main function

---
+++ src/__main__.py
@@ -0,0 +1,20 @@
+from main_for_mva_hmm_STA_12 import calculate_spatial_transient_wrapper
+
+def main():
+    plotting_flag=0
+    dt=0.05
+    min_track_length=25
+    plotting_saving_nice_image_flag=0
+    
+    image_saving_flag="svg"
+    image_saving_flag="tiff"
+
+    #folderpath1=r"C:\Users\miche\Desktop\simualted tracks\test_real_tracks"
+    #folderpath1=r"D:\photochromic_reversion_data\tst"
+    folderpath1=r"/Users/schulzp9/Documents/casta"
+
+    calculate_spatial_transient_wrapper(folderpath1, min_track_length, dt, plotting_flag, image_saving_flag)
+
+
+if __name__ == "__main__":
+    main()
